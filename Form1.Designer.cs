8ae51bec1b8ca78099502c076dd885fbd8ce3f0f
Даниил Азаров <azarov.daniil@inbox.ru>
2021-11-02 20:58:32 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace CreaterGraphs {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.addCircleButton = new System.Windows.Forms.Button();
+            this.addArcButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.findShortestPathButton = new System.Windows.Forms.Button();
+            this.shortestPathTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(745, 471);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            // 
+            // addCircleButton
+            // 
+            this.addCircleButton.Location = new System.Drawing.Point(12, 477);
+            this.addCircleButton.Name = "addCircleButton";
+            this.addCircleButton.Size = new System.Drawing.Size(125, 23);
+            this.addCircleButton.TabIndex = 2;
+            this.addCircleButton.Text = "Добавить вершину";
+            this.addCircleButton.UseVisualStyleBackColor = true;
+            this.addCircleButton.Click += new System.EventHandler(this.addCircleButton_Click);
+            // 
+            // addArcButton
+            // 
+            this.addArcButton.Location = new System.Drawing.Point(143, 477);
+            this.addArcButton.Name = "addArcButton";
+            this.addArcButton.Size = new System.Drawing.Size(125, 23);
+            this.addArcButton.TabIndex = 4;
+            this.addArcButton.Text = "Добавить дугу";
+            this.addArcButton.UseVisualStyleBackColor = true;
+            this.addArcButton.Click += new System.EventHandler(this.addLineButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(274, 477);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(125, 23);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // findShortestPathButton
+            // 
+            this.findShortestPathButton.Location = new System.Drawing.Point(405, 477);
+            this.findShortestPathButton.Name = "findShortestPathButton";
+            this.findShortestPathButton.Size = new System.Drawing.Size(156, 23);
+            this.findShortestPathButton.TabIndex = 6;
+            this.findShortestPathButton.Text = "Найти кратчайший путь";
+            this.findShortestPathButton.UseVisualStyleBackColor = true;
+            this.findShortestPathButton.Click += new System.EventHandler(this.findShortestPathButton_Click);
+            // 
+            // shortestPathTextBox
+            // 
+            this.shortestPathTextBox.Enabled = false;
+            this.shortestPathTextBox.Location = new System.Drawing.Point(567, 478);
+            this.shortestPathTextBox.Name = "shortestPathTextBox";
+            this.shortestPathTextBox.Size = new System.Drawing.Size(178, 23);
+            this.shortestPathTextBox.TabIndex = 1000000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 510);
+            this.Controls.Add(this.shortestPathTextBox);
+            this.Controls.Add(this.findShortestPathButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addArcButton);
+            this.Controls.Add(this.addCircleButton);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button addCircleButton;
+        private System.Windows.Forms.Button addArcButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button findShortestPathButton;
+        private System.Windows.Forms.TextBox shortestPathTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CreaterGraphs {
+    public partial class Form1 : Form {
+
+        List<Label> vertex = new List<Label>();
+        Dictionary<TextBox, List<Label>> line = new Dictionary<TextBox, List<Label>>();
+
+        Label previosClickedCircle = null;
+
+        Color keyPressed = Color.Blue;
+        Color keyReleased = ColorTranslator.FromHtml("#22ACDC");
+
+        bool isMouseDownLabel = false;
+        bool isMouseMovedLabel = false;
+        bool isAddCircle = true;
+        bool isAddLine = false;
+        bool isDelete = false;
+        bool isFindShortestPath = false;
+
+        enum isCheckedVertex {
+            NO,
+            YES
+        }
+
+        const int PATH_LENGHT = 0;
+        const int CHECKED_VERTEX = 1;
+
+        public Form1() {
+
+            InitializeComponent();
+            pictureBox.BackColor = Color.White;
+        }
+
+
+        private void circles_MouseDown(object sender, MouseEventArgs e) {
+            isMouseDownLabel = true;
+        }
+
+        private void circles_MouseUp(object sender, MouseEventArgs e) {
+            isMouseDownLabel = false;
+            Label clickedCircle = (Label)sender;
+
+            if (isDelete && !isMouseMovedLabel) {
+
+                List<TextBox> deletedLine = new List<TextBox>();
+
+                foreach (TextBox item in line.Keys) {
+                    if (line[item].Contains(clickedCircle)) {
+                        deletedLine.Add(item);
+                    }
+                }
+
+                foreach(TextBox item in deletedLine){
+                    deleteLine(item); 
+                }
+
+                deleteCircle(clickedCircle);
+            }
+
+            if (isAddLine && !isMouseMovedLabel) {
+
+                if (previosClickedCircle == null || previosClickedCircle == clickedCircle) {
+                    previosClickedCircle = clickedCircle;
+                    previosClickedCircle.BackColor = keyPressed;
+                    clickedCircle.BackColor = keyPressed;
+                    return;
+                }
+
+                foreach(TextBox item in line.Keys) {
+                    if (line[item].Contains(previosClickedCircle) && line[item].Contains(clickedCircle)) {
+                        previosClickedCircle = clickedCircle;
+                        return;
+                    }
+                }
+
+                clickedCircle.BackColor = keyReleased;
+                previosClickedCircle.BackColor = keyReleased;
+                addLine(clickedCircle);
+            }
+
+            if (isFindShortestPath && !isMouseMovedLabel) {
+                Label vertexClicked = (Label)sender;
+                const int BIG_VALUE = 2147483647;
+
+                if (previosClickedCircle == null || previosClickedCircle == vertexClicked) {
+                    previosClickedCircle = vertexClicked;
+                    previosClickedCircle.BackColor = keyPressed;
+                    vertexClicked.BackColor = keyPressed;
+                    return;
+                }
+
+                int[,] matrixLenght = fillMatrixLenght();
+                int[,] matrixReachability = fillMatrixReachability();
+                int[,] lenghtPathVertex = fillLenghtPathVertex(previosClickedCircle.TabIndex, vertexClicked.TabIndex);
+
+                lenghtPathVertex = findShortestPath(lenghtPathVertex, matrixLenght, previosClickedCircle.TabIndex, vertexClicked.TabIndex);
+
+                if (lenghtPathVertex[vertexClicked.TabIndex, 0] != BIG_VALUE) {
+                    shortestPathTextBox.Text = lenghtPathVertex[vertexClicked.TabIndex, 0].ToString();
+                } else {
+                    shortestPathTextBox.Text = (0).ToString();
+                }
+
+                previosClickedCircle.BackColor = keyReleased;
+                vertexClicked.BackColor = keyReleased;
+                previosClickedCircle = null;
+            }
+
+            isMouseMovedLabel = false;
+        }
+
+        private void circles_MouseMove(object sender, MouseEventArgs e) {
+            if (isMouseDownLabel) {
+                Point move = PointToClient(Control.MousePosition);
+
+                Label movedCircle = (Label)sender;
+
+                if (move.X > 0 && move.Y > -5 && move.X < (pictureBox.Size.Width - 30) &&
+                    move.Y < (pictureBox.Size.Height - 35)) {
+
+                    List<TextBox> movedLine = new List<TextBox>();
+
+                    foreach (TextBox item in line.Keys) {
+                        if (line[item].Contains(movedCircle)) {
+                            movedLine.Add(item);
+                        }
+                    }
+
+                    if (movedLine.Count >= 0) {
+                        Graphics gr = pictureBox.CreateGraphics();
+
+                        foreach (TextBox item in movedLine) {
+                            int x1 = line[item][0].Location.X;
+                            int y1 = line[item][0].Location.Y;
+                            int x2 = line[item][1].Location.X;
+                            int y2 = line[item][1].Location.Y;
+
+                            gr.DrawLine(new Pen(Color.White, 2), new Point(x1 + 20, y1 + 20), new Point(x2 + 20, y2 + 20));
+                        }
+
+                        movedCircle.Location = move;
+
+                        foreach (TextBox item in movedLine) {
+                            int x1 = line[item][0].Location.X;
+                            int y1 = line[item][0].Location.Y;
+                            int x2 = line[item][1].Location.X;
+                            int y2 = line[item][1].Location.Y;
+
+                            gr.DrawLine(new Pen(Color.Black, 2), new Point(x1 + 20, y1 + 20), new Point(x2 + 20, y2 + 20));
+
+                            item.Location = new Point((x1 + x2) / 2 + 17, (y1 + y2) / 2 + 17);
+                        }
+
+                    } else {
+                        movedCircle.Location = move;
+                    }
+
+                }
+
+                isMouseMovedLabel = true;
+            }
+
+        }
+
+        private void deleteCircle(Label clickedCircle) {
+            int index = vertex.IndexOf(clickedCircle);
+            vertex.Remove(clickedCircle);
+            Controls.Remove(clickedCircle);
+
+            for (int i = index; i < vertex.Count; i++) {
+                vertex[i].Text = Convert.ToString((i + 1));
+                vertex[i].TabIndex = (i + 1);
+            }
+        }
+
+
+        private void line_MouseClick(object sender, MouseEventArgs e) {
+
+            if (isDelete) {
+                deleteLine((TextBox)sender);
+            }
+
+        }
+
+        private void addLine(Label clickedCircle) {
+            int x1 = previosClickedCircle.Location.X;
+            int y1 = previosClickedCircle.Location.Y;
+            int x2 = clickedCircle.Location.X;
+            int y2 = clickedCircle.Location.Y;
+
+            Graphics gr = pictureBox.CreateGraphics();
+            gr.DrawLine(new Pen(Color.Black, 2), new Point(x1 + 20, y1 + 20), new Point(x2 + 20, y2 + 20));
+
+            TextBox textBox = new TextBox();
+            textBox.AutoSize = true;
+            textBox.Location = new Point((x1 + x2) / 2 + 17, (y1 + y2) / 2 + 17);
+            textBox.Size = new Size(20, 20);
+            textBox.TabIndex = line.Count();
+            textBox.BackColor = Color.White;
+            textBox.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox.Text = Convert.ToString(0);
+            textBox.ForeColor = Color.Black;
+            textBox.MouseClick += new MouseEventHandler(this.line_MouseClick);
+            textBox.TextChanged += new EventHandler(this.textBox_TextChanged);
+
+            List<Label> labelList = new List<Label>();
+            labelList.Add(previosClickedCircle);
+            labelList.Add(clickedCircle);
+
+            line.Add(textBox, labelList);
+
+            Controls.Add(textBox);
+            textBox.BringToFront();
+
+            previosClickedCircle = null;
+        }
+
+        private void deleteLine(TextBox deletedLine) {
+            int x1 = line[deletedLine][0].Location.X;
+            int y1 = line[deletedLine][0].Location.Y;
+            int x2 = line[deletedLine][1].Location.X;
+            int y2 = line[deletedLine][1].Location.Y;
+
+            Graphics gr = pictureBox.CreateGraphics();
+            gr.DrawLine(new Pen(Color.White, 2), new Point(x1 + 20, y1 + 20), new Point(x2 + 20, y2 + 20));
+
+            line.Remove(deletedLine);
+            Controls.Remove(deletedLine);
+        }
+
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e) {
+            if (!isMouseDownLabel && isAddCircle && (e.Location.X > 0 && e.Location.Y > -5 &&
+                e.Location.X < (pictureBox.Size.Width - 30) && e.Location.Y < (pictureBox.Size.Height - 35))) {
+
+                Label label = new Label();
+                label.AutoSize = true;
+                label.Location = e.Location;
+                label.Size = new Size(30, 30);
+                label.TabIndex = vertex.Count();
+                label.BackColor = keyReleased;
+                label.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+                label.Text = Convert.ToString(vertex.Count() + 1);
+                label.MouseDown += new MouseEventHandler(this.circles_MouseDown);
+                label.MouseUp += new MouseEventHandler(this.circles_MouseUp);
+                label.MouseMove += new MouseEventHandler(this.circles_MouseMove);
+
+                GraphicsPath path = new GraphicsPath();
+                path.AddEllipse(0, 5, label.Width, label.Height);
+
+                label.Region = new Region(path);
+
+                vertex.Add(label);
+
+                this.Controls.Add(label);
+                label.BringToFront();
+            }
+        }
+
+
+        private void addCircleButton_Click(object sender, EventArgs e) {
+            isAddCircle = true;
+            isAddLine = false;
+            isDelete = false;
+            isFindShortestPath = false;
+        }
+
+        private void addLineButton_Click(object sender, EventArgs e) {
+            isAddCircle = false;
+            isAddLine = true;
+            isDelete = false;
+            isFindShortestPath = false;
+
+            previosClickedCircle = null;
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e) {
+            isAddCircle = false;
+            isAddLine = false;
+            isDelete = true;
+            isFindShortestPath = false;
+        }
+
+        private void findShortestPathButton_Click(object sender, EventArgs e) {
+            isAddCircle = false;
+            isAddLine = false;
+            isDelete = false;
+            isFindShortestPath = true;
+
+            previosClickedCircle = null;
+        }
+
+
+        private int[,] fillMatrixLenght() {
+            int[,] matrix = new int[vertex.Count, vertex.Count];
+
+            for (int i = 0; i < vertex.Count; i++) {
+                for (int j = 0; j < vertex.Count; j++) {
+                    matrix[i, j] = -1;
+                }
+            }
+
+            foreach (TextBox item in line.Keys) {
+                int vertexIndex1 = line[item][0].TabIndex;
+                int vertexIndex2 = line[item][1].TabIndex;
+
+                matrix[vertexIndex1, vertexIndex2] = Convert.ToInt32(item.Text);
+                matrix[vertexIndex2, vertexIndex1] = Convert.ToInt32(item.Text);
+            }
+
+            return matrix;
+        }
+
+        private int[,] fillMatrixReachability() {
+            int[,] matrix = new int[vertex.Count, vertex.Count];
+
+            for (int i = 0; i < vertex.Count; i++) {
+                for (int j = 0; j < vertex.Count; j++) {
+                    matrix[i, j] = 0;
+                }
+            }
+
+            foreach (TextBox item in line.Keys) {
+                int vertexIndex1 = line[item][0].TabIndex;
+                int vertexIndex2 = line[item][1].TabIndex;
+
+                matrix[vertexIndex1, vertexIndex2] = 1;
+                matrix[vertexIndex2, vertexIndex1] = 1;
+            }
+
+            return matrix;
+        }
+
+        private int[,] fillLenghtPathVertex(int startVertex, int endVertex) {
+            int[,] lenghtPathVertex = new int[vertex.Count, 2];
+
+            const int BIG_VALUE = 2147483647;
+
+            for (int i = 0; i < vertex.Count; i++) {
+                lenghtPathVertex[i, PATH_LENGHT] = BIG_VALUE;
+                lenghtPathVertex[i, CHECKED_VERTEX] = ((int)isCheckedVertex.NO);
+            }
+
+            lenghtPathVertex[startVertex, PATH_LENGHT] = 0;
+
+            return lenghtPathVertex;
+        }
+
+        private int[,] findShortestPath(int[,] lenghtPathVertex, int[,] matrix, int startVertex, int endVertex) {
+            for (int i = 0; i < vertex.Count; i++) {
+
+                if( (matrix[startVertex, i] > -1) && (lenghtPathVertex[i, 0] > (matrix[startVertex, i] + lenghtPathVertex[startVertex, 0])) ) {
+
+                    lenghtPathVertex[i, 0] = matrix[startVertex, i] + lenghtPathVertex[startVertex, 0];
+
+                    lenghtPathVertex = findShortestPath(lenghtPathVertex, matrix, i, endVertex);
+                }
+
+            }
+
+            return lenghtPathVertex;
+        }
+
+
+        private void textBox_TextChanged(object sender, EventArgs e) {
+            TextBox textBox = (TextBox)sender;
+            string value = textBox.Text.ToString();
+
+            if (value.Length > 0) {
+                try {
+                    int i = Convert.ToInt32(value);
+                } catch (FormatException) {
+                    textBox.Text = "";
+
+                    foreach (char item in value) {
+                        if (Char.IsDigit(item)) {
+                            textBox.Text += item;
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+
+}
